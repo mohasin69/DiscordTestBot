75317a3635ba43e7c380bd1ed186910fef307dff
Shaikh Mohasin <mohasin69@gmail.com>
2018-05-24 14:27:09 +0530

    tryouts

--- bot.js
+++ bot.js
@@ -1,15 +1,43 @@
+var Discord = require('discord.io');
+var logger = require('winston');
 
-const Discord = require("Discord.js");
-const client = new Discord.Client();
-client.on('ready', () => {
-    console.log("I am ready!");
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: process.env.BOT_TOKEN,
+   autorun: true
 });
 
-client.on('message', () => {
-    if(message.content === 'ping')
-    {
-        message.reply('pong');
-    }
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
 });
 
-client.login(process.env.BOT_TOKEN);
+
+//client.login(process.env.BOT_TOKEN);
